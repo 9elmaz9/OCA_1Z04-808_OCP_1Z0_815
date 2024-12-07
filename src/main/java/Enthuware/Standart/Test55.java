88ecf9b25ff87f666b606b9c5d60c64b64ab920c
Dzhelianchyk <kakbyelmaz@gmail.com>
2024-12-07 22:55:38 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/Test55.java
@@ -0,0 +1,15 @@
+package Enthuware.Standart;
+
+public class Test55 {
+}
+/*
+*
+Given the following code, which of the constructors shown in the options can be added to class B without causing a compilation to fail?
+class A{
+   int i;
+   public A(int x) { this.i = x; }
+}
+class B extends A{
+   int j;
+   public B(int x, int y) { super(x);  this.j = y; }
+}*/
