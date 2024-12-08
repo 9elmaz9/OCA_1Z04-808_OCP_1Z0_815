e5840063855622cd22310846b3fcc1d318436798
Dzhelianchyk <kakbyelmaz@gmail.com>
2024-12-08 02:37:40 +0100

    les18

--- src/main/java/com/just/Lesson18/Test1.java
+++ src/main/java/com/just/Lesson18/Test1.java
@@ -10,6 +10,7 @@ public class Test1 {
         double[][] array3;
         int[][] array10;
 
+
         // can't do  int [5] array;
 
 
@@ -28,5 +29,39 @@ public class Test1 {
         //LIKE THIS NOT OKEY
         //array10 = new int[][3];
 
+
+        //выводим длинну массива array
+        System.out.println(array2.length);
+        //lenght это не метод , это переменная класса , атрибут класса
+
+
+        //ИНИЦИАЛИЗАЦИЯ СТАТИЧЕСКИМ МЕТОДОМ
+        // String[] array2;
+        array2[0] = "privet";
+        array2[1] = "poka";
+        array2[2] = "ok";
+
+        // double[][] array3; содержит 4 одномерных массива длина каждого равна двум
+        array3[0][0] = 3.14;
+        array3[2][1] = 3.14;
+
+        double[] array5;
+        array5 = new double[2];
+        array5[0] = 2.5;
+        array5[1] = 3.5;
+
+// присваиваем 2.5 и 3.5 нашему  array3 его индексу 1
+        array3[1] = array5;
+        System.out.println(array3[1][0]); //2.5
+
+        //
+        double[] array7;
+        array7 = new double[2];
+
+        array7 = array5;  // это значит что   array7 будет ссылатсья на обьект array5
+
+
+
+        //ДИНАМИЧЕСКАЯ ИНИЦИАЛИЗАЦИЯ В  КЛАССЕ Test2
     }
 }
