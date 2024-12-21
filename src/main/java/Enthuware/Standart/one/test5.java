664a0cac4b55aa0944dc7d9c048fda6d9223631d
Dzhelianchyk <kakbyelmaz@gmail.com>
2024-12-21 23:26:52 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/one/test5.java
@@ -0,0 +1,14 @@
+package Enthuware.Standart.one;
+
+public class test5 {
+}
+/*
+Given:
+String mStr = "123";
+long m = // 1
+        Which of the following options when put at //1 will assign 123 to m?*/
+
+
+//new Long(mStr);
+//Long.valueOf(mStr).longValue();
+//Long.parseLong(mStr);
