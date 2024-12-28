99087719f8f5b585e06dc30da2a7c1a5e87d3753
Dzhelianchyk <kakbyelmaz@gmail.com>
2024-12-28 23:09:11 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/one/test8.java
@@ -0,0 +1,13 @@
+package Enthuware.Standart.one;
+
+public class test8 {
+}
+/**
+ * ! Left Unanswered You had to select 1 option
+ *
+ * long y = 123_456_L;
+ * An underscore can only occur in between two digits. So the _ before L is invalid.*/
+//float f2 = 123_345_667F;
+
+/*
+* You may use underscore for all kinds of numbers including long, double, float, binary, as well as hex,For example, the following are all valid numbers - int hex = 0xCAFE_BABE; float f = 9898_7878.333_333f; int bin = 0b1111_0000_1100_1100;*/
