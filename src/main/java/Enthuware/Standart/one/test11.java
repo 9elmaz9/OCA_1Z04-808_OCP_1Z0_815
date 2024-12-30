35f8ccc0dee17daa3f477cfe2496d4f82266a6f6
Dzhelianchyk <kakbyelmaz@gmail.com>
2024-12-30 21:56:31 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/one/test11.java
@@ -0,0 +1,28 @@
+package Enthuware.Standart.one;
+
+public class test11 {
+}
+/**
+ *
+ * Given:
+ *
+ *
+ * package strings;
+ * public class StringFromChar {
+ *
+ *     public static void main(String[] args) {
+ *         String myStr = "good";
+ *         char[] myCharArr = {'g', 'o', 'o', 'd' };
+ *
+ *         String newStr = null;
+ *         for(char ch : myCharArr){
+ *             newStr = newStr + ch;
+ *         }
+ *
+ *         System.out.println((newStr == myStr)+ " " + (newStr.equals(myStr)));
+ *
+ *     }
+ * }
+ *
+ * What will it print when compiled and run?*/
+//false false
