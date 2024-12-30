a4066676997defd31ba6fd1037daa99ee7e11193
Dzhelianchyk <kakbyelmaz@gmail.com>
2024-12-30 21:55:46 +0100

    enthuware

--- src/main/java/Enthuware/Standart/one/test10.java
+++ src/main/java/Enthuware/Standart/one/test10.java
@@ -6,6 +6,6 @@ public class test10 {
  * Which of the following are valid at line 1?  public class X{     //line 1: insert code here. }*/
 
 //String s;
-//
+
 //String s = this.toString();
 //Since every class directly or indirectly extends Object class and since Object class has a toString() method, that toString() method will be invoked and the String that it returns will be assigned to s.
