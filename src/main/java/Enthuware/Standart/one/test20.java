e27a2c580c9bfa2feaa3d1cc709eb3b8daf8fc52
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-01-08 23:54:44 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/one/test20.java
@@ -0,0 +1,11 @@
+package Enthuware.Standart.one;
+
+public class test20 {
+}
+//An instance member ...
+
+//can be a variable, a constant or a method.
+
+//belongs to an instance of the class.
+
+//An instance member belongs to a single instance, not the class as a whole. An instance member is a member variable or a member method that belongs to a specific object instance. All non-static members are instance members.
