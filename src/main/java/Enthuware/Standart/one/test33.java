02595b5e3690482a8bef12c14ada0c2cf385eaab
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-01-11 22:44:33 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/one/test33.java
@@ -0,0 +1,17 @@
+package Enthuware.Standart.one;
+
+public class test33 {
+}
+//Is it possible to create arrays of length zero?
+
+//Yes, you can create arrays of any type with length zero.
+/*
+*
+Yes, you can create arrays of any type with length zero.
+Java allows arrays of length zero to be created.
+* Here is an example: int[] zeroLengthArray1 = new int[0];
+*  System.out.println(zeroLengthArray1.length);
+* //will print 0 String[] zeroLengthArray2 = new String[0];
+*  System.out.println(zeroLengthArray2.length);//will print 0*/
+
+//Example: When a Java program is run without any program arguments, the String[] args argument to main() gets an array of length Zero.
