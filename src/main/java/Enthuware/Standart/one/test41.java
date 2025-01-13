930e4ab4dd72afe7836402a7ae2efd30e30a0b97
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-01-13 23:59:26 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/one/test41.java
@@ -0,0 +1,8 @@
+package Enthuware.Standart.one;
+
+public class test41 {
+}
+//Which of the following are true regarding the new Date-Time API of Java 8?
+
+//It uses the calendar system defined in ISO-8601 as the default calendar.
+//Most of the actual date related classes in the Date-Time API such as LocalDate, LocalTime, and LocalDateTime are immutable.
