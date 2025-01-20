dee11596e9c15d49d130fabc57354225feddb4d0
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-01-21 00:12:17 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/one/test75.java
@@ -0,0 +1,36 @@
+package Enthuware.Standart.one;
+
+public class test75 {
+}
+//Which of the following statements are correct?
+
+
+//An abstract class can be extended by an abstract or a concrete class.
+//A concrete class can be extended by an abstract or a concrete class.
+//An interface can be extended by another interface.
+
+/**
+ * Правильные утверждения:
+ *
+ * An abstract class can be extended by an abstract or a concrete class.
+ *
+ * Абстрактный класс может быть расширен (унаследован) как абстрактным, так и конкретным (неабстрактным) классом.
+ * A concrete class can be extended by an abstract or a concrete class.
+ *
+ * Конкретный класс (класс с реализацией) может быть расширен как конкретным, так и абстрактным классом.
+ * An interface can be extended by another interface.
+ *
+ * Интерфейсы в Java могут расширять другие интерфейсы, это позволяет наследовать методы и поля интерфейса.
+ * Остальные утверждения:
+ * An interface can be extended by an abstract class.
+ * Неверно. Интерфейсы не могут быть расширены классами (ни абстрактными, ни конкретными).
+ * Классы могут реализовывать интерфейсы, но не расширять их.
+ *
+ * An interface can be extended by a concrete class.
+ * Неверно. То же самое — классы реализуют интерфейсы, но не расширяют их.
+ *
+ * An abstract class cannot implement an interface.
+ * Неверно. Абстрактные классы могут реализовывать интерфейсы (и при этом не обязаны предоставлять реализации всех методов интерфейса, это сделает их подкласс).
+ *
+ * Итог:
+ * Выберите первые три варианта.*/
