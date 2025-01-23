540418d34976449d1ee56a804e5b46fec3b2e45a
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-01-24 00:02:08 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test1/test_5.java
@@ -0,0 +1,14 @@
+package Enthuware.Standart.test1;
+
+public class test_5 {
+}
+/**Given: String mStr = "123"; long m = // 1 Which of the following options when put at //1 will assign 123 to m?*
+ *
+ */
+
+//new Long(mStr);
+
+//Long.parseLong(mStr);
+
+
+//Long.valueOf(mStr).longValue();   Long.valueOf(mStr) returns a Long object containing 123. longValue() on the Long object returns 123.
