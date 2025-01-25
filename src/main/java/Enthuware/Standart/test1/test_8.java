96f15d7e8dd8932868e875fcfcde5181e22734c0
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-01-25 23:57:13 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test1/test_8.java
@@ -0,0 +1,11 @@
+package Enthuware.Standart.test1;
+
+public class test_8 {
+}
+//Identify the valid code fragments when occurring by themselves within a method.
+
+
+//float f2 = 123_345_667F;
+
+/*You may use underscore for all kinds of numbers including long, double, float, binary, as well as hex.
+For example, the following are all valid numbers - int hex = 0xCAFE_BABE; float f = 9898_7878.333_333f; int bin = 0b1111_0000_1100_1100;*/
