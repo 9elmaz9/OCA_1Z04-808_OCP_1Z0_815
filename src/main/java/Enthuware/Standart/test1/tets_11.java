61248a0d9a8815aec362862a92653652315e8e63
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-01-26 20:37:57 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test1/tets_11.java
@@ -0,0 +1,24 @@
+package Enthuware.Standart.test1;
+
+public class tets_11 {
+}
+/**
+ * Given:   package strings;
+ * public class StringFromChar {
+ * public static void main(String[] args) {
+ * String myStr = "good";
+ * char[] myCharArr = {'g', 'o', 'o', 'd' };
+ * String newStr = null;
+ * for(char ch : myCharArr){
+ * newStr = newStr + ch;
+ * }
+ * System.out.println((newStr == myStr)+ " " + (newStr.equals(myStr)));
+ * } }
+ * What will it print when compiled and run?*/
+
+//false false
+
+/**Since newStr is null at the beginning, the first iteration of the loop assigns "nullg" to newStr.
+ *  Therefore, at the end of the loop, newStr is actually "nullgood". Had newStr been defined as String newStr = "";
+ *  then the program would have printed false for newStr == myStr because both the references are pointing to two different objects,
+ * and true for newStr.equals(myStr) because both the objects contain the exact same String.*/
