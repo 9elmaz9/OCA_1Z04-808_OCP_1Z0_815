bd0f977baba232cccf16dbd791c14ff16e922baa
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-01-28 23:29:54 +0100

    enthuware

--- src/main/java/Enthuware/Standart/test1/test_16.java
+++ src/main/java/Enthuware/Standart/test1/test_16.java
@@ -2,27 +2,21 @@ package Enthuware.Standart.test1;
 
 public class test_16 {
 }
-/**
- * What will the following code print when compiled and run?
- * public class OrderTest {
- * public void initData(String[] arr){
- * int ind = 0;
- * for(String str : arr){
- * str.concat(str+" "+ind);             ind++;
- * }
- * }
- * public void printData(String[] arr){
- * for(String str : arr){
- * System.out.println(str);
- * }
- * }
- * public static void main(String[] args) {
- * OrderTest ot = new OrderTest();
- * String[] arr = new String[2];
- * ot.initData(arr);
- * ot.printData(arr);
- * } }*/
+/**What will the following code print?
+ * public class Test{
+ *  public static void testInts(Integer obj, int var){
+ *  obj = var++;
+ *  obj++;
+ *  }
+ *  public static void main(String[] args) {
+ *
+ *  Integer val1 = new Integer(5);
+ *  int val2 = 9;
+ *  testInts(val1++, ++val2);
+ *  System.out.println(val1+" "+val2);
+ *  }
+ *  }  */
 
-//It will throw a RuntimeException at run time.
 
-/*When you do new String[2], you create a String array of two elements. arr is therefore not null. But each element of the array is not given any value and is therefore null. When you call a method on that element (i.e. str.concat(str+" "+ind); in initData), it will generate a NullPointerException, which is a RuntimeException.*/
+//6 10
+/*There are multiple concepts at play here: 1. All the wrapper objects are immutable. So when you do obj++, what actually happens is something like this: obj = new Integer( obj.intValue()  + 1);    2.val1++ uses post-increment operator, which implies that you note down the current value of val1, increment it, and then pass the original noted down value to the method testInts. Thus, the reference value of Integer 5 is passed to testInts. But val1 is set to point to a new Integer object containing 6. ++val2 uses pre-increment operator, which implies that you first increment val2 and then pass the incremented value. Therefore, val2 is incremented to 10 and then 10 is passed to the method testInts.  3. Java uses pass by value semantics in method calls. In case of primitive variables, their values are passed, while in case of Objects, their reference values are passed.  Thus, when you assign a different object to reference variable in a method, the original reference variable that was passed from the calling method still points to the same object that it pointed to before the call. In this question, therefore, val1 in main still points to 6 after the call to testInts returns.*/
---
+++ src/main/java/Enthuware/Standart/test1/test_17.java
@@ -0,0 +1,4 @@
+package Enthuware.Standart.test1;
+
+public class test_17 {
+}
