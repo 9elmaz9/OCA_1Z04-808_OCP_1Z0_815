c712c92b01790647f4bf1acaf33497c91a659d03
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-01-31 23:33:08 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test1/test_24.java
@@ -0,0 +1,13 @@
+package Enthuware.Standart.test1;
+
+public class test_24 {
+}
+/**The following code snippet will print true.
+ * String str1 = "one";
+ * String str2 = "two";
+ * System.out.println( str1.equals(str1=str2) )
+ * ;*/
+
+//False
+
+//First the value of 'str1' is evaluated (i.e. one). Now, before the method is called, the operands are evaluated, so str1 becomes "two". so "one".equals("two") is false.
