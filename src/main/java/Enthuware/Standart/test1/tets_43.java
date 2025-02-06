6637526d63b82b2eb09fc31a3e2b2e9eeb428aea
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-02-07 00:24:40 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test1/tets_43.java
@@ -0,0 +1,21 @@
+package Enthuware.Standart.test1;
+
+public class tets_43 {
+}
+/**What can be inserted in the code below so that it will print true when run?
+ *  public class TestClass{
+ *  public static boolean checkList(List list, Predicate<List> p){
+ *  return p.test(list);
+ *  }
+ *  public static void main(String[] args) {
+ *  boolean b = //WRITE CODE HERE
+ *  System.out.println(b);    } }
+ *  */
+
+
+//checkList(new ArrayList(), al -> al.isEmpty());
+/*The test method of Predicate returns a boolean. So all you need for your  body part in your lambda expression is an expression that returns a boolean. isEmpty() is a valid method of ArrayList, which returns true if there are no elements in the list. Therefore, al.isEmpty() constitutes a valid body for the lambda expression in this case.*/
+
+
+//checkList(new ArrayList(), al -> al.add("hello"))
+/*The add method of ArrayList returns a boolean. Further, it returns true if the list is altered because of the call to add. In this case, al.add("hello") indeed alters the list because a new element is added to the list.*/
