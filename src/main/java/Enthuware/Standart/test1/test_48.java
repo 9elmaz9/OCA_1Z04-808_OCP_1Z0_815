aa317cdfcf2a8fb038db8f1bbc70fbc4fbd85bad
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-02-09 00:01:56 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test1/test_48.java
@@ -0,0 +1,32 @@
+package Enthuware.Standart.test1;
+
+public class test_48 {
+}
+/**Consider the directory structure shown in Image 1 that displays available folders and classes and the code given below:
+ *  class StockQuote{
+ *  Stock stock;
+ *  public StockQuote(Stock s)  {
+ *    }
+ *  public void store() throws IOException{
+ *  Util.store(stock);
+ *  }
+ *  public double computePrice(){
+ *
+ *  return Helper.getPricer(stock).price();
+ *  }
+ *  }
+ *  Assuming that the code uses valid method calls, what statements MUST be added to the above class?*/
+
+
+//import com.enthu.*;
+
+
+//package com.enthu.rad;
+/*Since there is no import statement available for com.enthu.rad package,
+you must put the given class in com.enthu.rad package so that it will be accessible. Classes of the same package are always available to each other.*/
+
+
+//import com.*;
+
+
+//import java.io.*;
