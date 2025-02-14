00fb4dbf6df4147327df273ca13c4da0fae9aecd
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-02-14 23:59:54 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test2/test3.java
@@ -0,0 +1,13 @@
+package Enthuware.Standart.test2;
+
+public class test3 {
+}
+/**Consider the following class:
+
+ public class Test{
+ public int id;
+ }
+ Which of the following is the correct way to make the variable 'id' read only for any other class?*/
+
+//Make 'id' private and provide a public method getId() which will return its value.
+//This is a standard way of providing read only access to internal variables.
