11c6858621c4dcb3af43eed2dc7dba8ae1faa353
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-02-28 00:12:01 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test2/test31.java
@@ -0,0 +1,13 @@
+package Enthuware.Standart.test2;
+
+public class test31 {
+}
+//Which of the following are valid declarations inside an interface independent of each other?
+
+
+//void compute();
+/*All interface methods have to be public. No access control keyword in the method declaration also means public in an interface.
+ (Note that the absence of access control keyword in the method declaration in a class means package protected.)*/
+
+
+//public void compute();
