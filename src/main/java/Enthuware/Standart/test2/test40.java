1e07bb28b53ee51a35c0fb56da1eb1f4d3ecec0d
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-03-02 23:58:10 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test2/test40.java
@@ -0,0 +1,20 @@
+package Enthuware.Standart.test2;
+
+public class test40 {
+}
+/**Consider the following lines of code:
+ *  Integer i = new Integer(42);
+ *  Long ln = new Long(42);
+ *  Double d = new Double(42.0);
+ *  Which of the following options are valid code fragments?*/
+
+
+//i.equals(d);
+
+
+//d.equals(ln);
+
+
+
+//
+//ln.equals(42);
