571da13fa74691ca5c27e8cbde73663f31cbd0bc
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-03-05 23:57:42 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test2/test53.java
@@ -0,0 +1,10 @@
+package Enthuware.Standart.test2;
+
+public class test53 {
+}
+//When a class, whose members should be accessible only to members of that class,
+// is coded such a way that its members are accessible to other classes as well, this is called ...
+
+//weak encapsulation
+
+//When a class is properly encapsulated, only the members that are part of its public API are publicly accessible to other classes. Rest is all private.
---
+++ src/main/java/Enthuware/Standart/test2/test54.java
@@ -0,0 +1,24 @@
+package Enthuware.Standart.test2;
+
+public class test54 {
+}
+
+/**Which of the given options can be successfully inserted at line 1....
+ *  //line 1    public class A{
+ *
+ *  }*/
+
+
+//import java.lang.*;
+//Although this package is automatically imported, it is not an error to import it explicitly.
+
+
+
+//package p.util;
+//It is a perfectly valid package statement.
+
+
+//abstract class MyClass{ }
+//You can have more than one classes in a file but at most one of them can be public.
+
+//To make a class abstract, you only need to mark it abstract as shown in Option 4. You don't necessarily need to put an abstract method in a class.
