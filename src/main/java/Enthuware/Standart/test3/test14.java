e843545e960511657da2309b9c2273c0741111c2
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-03-13 23:59:34 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test3/test14.java
@@ -0,0 +1,9 @@
+package Enthuware.Standart.test3;
+
+public class test14 {
+}
+//    Which of the following code fragments are valid method declarations?
+
+//    void method2( ) { }
+
+//    A valid method declaration MUST specify a return type, all other things are optional.
