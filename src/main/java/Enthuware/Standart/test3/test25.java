2e5a04f2adabe3338292b7318be60f7c4bd6cd07
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-03-15 00:04:57 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test3/test25.java
@@ -0,0 +1,23 @@
+package Enthuware.Standart.test3;
+
+public class test25 {
+}
+
+/**What is the effect of compiling and running the code shown in exhibit?
+
+ public class TestClass{
+ public static void main (String args []){
+ int sum = 0;
+ for (int i = 0, j = 10; sum > 20; ++i, --j)      // 1
+ {
+ sum = sum+ i + j;
+ }
+ System.out.println("Sum = " + sum);
+ }
+ }
+ */
+
+//It will print Sum = 0
+//Note that the loop condition is sum >20 and not sum <20.
+
+//Read the questions carefully. This is very important. Some questions are easy but you need to read them carefully.
