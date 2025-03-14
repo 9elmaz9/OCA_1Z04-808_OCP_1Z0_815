d03db23a2a67749fe210116e2e8f19be123e6abd
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-03-14 23:59:30 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test3/test18.java
@@ -0,0 +1,22 @@
+package Enthuware.Standart.test3;
+
+public class test18 {
+}
+/**Consider the following code:
+ *  public class TestClass {
+ *  //define tester method here
+ *  public static void main(String[] args) throws Exception {
+ *  TestClass tc = new TestClass();
+ *  while(tc.tester()){
+ *  System.out.println("running...");
+ *  }
+ *  } }
+ *  Which of the following options would be a valid implementation of tester() method?*/
+
+/*public boolean tester(){
+ return false;
+ }*/
+
+
+/*public Boolean tester(){ return false; }*/
+
