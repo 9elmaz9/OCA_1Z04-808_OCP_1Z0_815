f633eee893e10993e08eb8c257b752943cbaa14b
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-03-16 23:52:02 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test3/test29.java
@@ -0,0 +1,15 @@
+package Enthuware.Standart.test3;
+
+public class test29 {
+}
+//    Which of the following are benefits of polymorphism?
+
+//    It makes the code more reusable.
+
+//    It makes the code more dynamic.
+
+/*
+It makes the code more dynamic.
+Polymophism allows the actual decision of which method is to be invoked to be taken at runtime based on the actual class of object.
+This is dynamic binding and makes the code more dynamic.*/
+
