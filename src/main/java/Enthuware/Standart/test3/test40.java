826e9b6e2d3df3ff8fab2d586428611c9485bb39
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-03-21 00:00:02 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test3/test40.java
@@ -0,0 +1,25 @@
+package Enthuware.Standart.test3;
+
+public class test40 {
+}
+/**What will be result of attempting to compile the following code appearing in TestClass.java file?
+ *
+ *  import java.util.*;
+ *  package test;
+ *  public class TestClass{
+ *  public OtherClass oc = new OtherClass();
+ *  }
+ *  class OtherClass{
+ *  int value;
+ *  }
+ *  */
+
+//The class will fail to compile .
+
+//The package declaration can never occur after an import statement.
+
+/**The order is:
+ package statement.
+ import statements
+ class/ interface definitions.
+ Important point to note here is YOU MUST READ THE QUESTIONS VERY CAREFULLY.*/
