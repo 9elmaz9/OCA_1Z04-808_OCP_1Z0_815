d4311daec68263b7798e402186635313739abeca
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-03-24 23:42:58 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test3/test44.java
@@ -0,0 +1,19 @@
+package Enthuware.Standart.test3;
+
+public class test44 {
+}
+//    Which of the following statements are true?
+
+
+//System.out.println(1 + 2 + "3"); would print 33.
+//operator + is left associative so evaluation of (1 + 2 + "3" ) is as follows: ( 1 + 2 ) + "3" -> 3 + "3" -> "33".
+
+
+
+//System.out.println(4 + 1.0f); would print 5.0
+//(4 + 1.0f ) evaluates as 4.0f + 1.0f ->5.0f -> 5.0
+
+/**All operands of type byte, char or short are promoted AT LEAST to an int before performing mathematical operations.
+ * If one of the operands is larger than an int then the other one is promoted to the same type.
+ * Note that System.out.println((float)5/4); will print 1.25. If you remove the explicit cast (float), it will print 1.*/
+
