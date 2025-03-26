806838ab3b7bfa80d34efbaac5172b59acbc4a04
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-03-27 00:00:47 +0100

    enthuware

---
+++ src/main/java/Enthuware/Standart/test3/test56.java
@@ -0,0 +1,14 @@
+package Enthuware.Standart.test3;
+
+public class test56 {
+}
+/**Given the following line of code:
+ *  List students = new ArrayList();
+ *  Identify the correct statement:*/
+
+
+//The reference type is List and the object type is ArrayList.
+
+/**Since you are doing new ArrayList, you are creating an object of class ArrayList.
+ * You are assigning this object to variable "students",
+ * which is declared of class List. Reference type means the declared type of the variable.*/
