bc07bbf4c5b8af6e2aeb9782ae0500f6967c8e0a
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-03-29 20:16:34 +0100

    enthuware

--- src/main/java/Enthuware/Standart/test4/test10.java
+++ src/main/java/Enthuware/Standart/test4/test10.java
@@ -2,3 +2,14 @@ package Enthuware.Standart.test4;
 
 public class test10 {
 }
+//    Which of the following are correct about "encapsulation"?
+
+
+//    It helps make sure that clients have no accidental dependence on the choice of representation
+
+
+//    It helps avoiding name clashes as internal variables are not visible outside.
+
+   /** Encapsulation is the technique used to package the information in such a way as to hide what should be hidden,
+    * and make visible what is intended to be visible. In simple terms, encapsulation generally means making the data
+    * variables private and providing public accessors.*/
--- src/main/java/Enthuware/Standart/test4/test9.java
+++ src/main/java/Enthuware/Standart/test4/test9.java
@@ -2,3 +2,19 @@ package Enthuware.Standart.test4;
 
 public class test9 {
 }
+/**Consider the following program:
+ *  public class TestClass{
+ *  public static void main(String[] args){
+ *  String tom = args[0];
+ *  String dick = args[1];
+ *  String harry = args[2];
+ *  System.out.println(harry);
+ *  } }
+ *  What will be printed if the program is run using the command line:
+ *  java TestClass 111 222 333*/
+
+
+
+//333
+
+//java and classname are not part of the args array. So tom gets "111", dick gets "222" and harry gets "333".
