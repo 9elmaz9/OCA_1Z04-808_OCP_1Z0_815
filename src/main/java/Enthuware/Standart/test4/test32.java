285a6188734d355c5636a251964a2f1357051877
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-03 00:00:25 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test4/test32.java
@@ -0,0 +1,18 @@
+package Enthuware.Standart.test4;
+
+public class test32 {
+}
+
+//    Which of the following methods can be called on a String object?
+
+
+//    substring(int i)
+//returns substring starting from i to end.
+
+//    substring(int i, int j)
+//returns substring starting from i to j-1.
+
+
+//    equals(Object o)
+
+//    Since Object class has this method, every java class inherits it.
