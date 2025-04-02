c08904d258abf1619e36295da4a28b9ef3e9a3a2
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-03 00:01:17 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test4/test33.java
@@ -0,0 +1,17 @@
+package Enthuware.Standart.test4;
+
+public class test33 {
+}
+//    Which of these statements are valid when occurring by themselves in a method?
+
+
+//do { break ; } while (true) ;
+
+//switch (1) { default : break; }
+//You can use a constant in switch(...);
+
+
+//for ( ; true ; ) break ;
+
+//    It is not possible to break out of an if statement. But if the if statement is placed within a switch statement or a loop construct, the usage of break in option 3 would be valid.
+
