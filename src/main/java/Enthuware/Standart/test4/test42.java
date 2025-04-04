6459f6999f95fc49cf1da9bf826485ac10bc21b1
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-05 00:00:56 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test4/test42.java
@@ -0,0 +1,11 @@
+package Enthuware.Standart.test4;
+
+public class test42 {
+}
+//    Which of these are NOT legal declarations within a class?
+
+
+//    abstract int t ;
+/**
+ abstract int t ;
+ Variables can't be declared as abstract or native.*/
