4307811f7659b9300a4811b36a6fb3ff1d25d275
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-05 00:00:25 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test4/test40.java
@@ -0,0 +1,9 @@
+package Enthuware.Standart.test4;
+
+public class test40 {
+}
+/**What will the following statement return?
+ * "    hello java guru   ".trim();*/
+
+
+//"hello java guru"
---
+++ src/main/java/Enthuware/Standart/test4/test41.java
@@ -0,0 +1,25 @@
+package Enthuware.Standart.test4;
+
+public class test41 {
+}
+/**Given: public class Bandwidth{
+ *  public int available = 0;
+ *  public int getAvailable(){
+ *  return available;
+ *  }
+ *  public Bandwidth(int quota){
+ *  this.available = quota;
+ *  }
+ *  public void addMore(int more){
+ *  available += more;
+ *  }
+ *  } and another piece of code from another class:
+ *  Bandwidth bw = new Bandwidth(100);
+ *  //INSERT CODE HERE
+ *
+ *  System.out.println(bw.getAvailable());
+ *
+ *  What can be inserted in the code above so that it will print 0?*/
+
+//bw.available = 0;
+
