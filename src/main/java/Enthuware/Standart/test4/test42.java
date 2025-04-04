7484a0b6e39fbe3bd4229b33378ca0dc1735c081
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-05 00:01:23 +0200

    enthuware

--- src/main/java/Enthuware/Standart/test4/test42.java
+++ src/main/java/Enthuware/Standart/test4/test42.java
@@ -9,3 +9,8 @@ public class test42 {
 /**
  abstract int t ;
  Variables can't be declared as abstract or native.*/
+
+/**static and final are valid modifiers for both member field and method declarations within a class.
+ * transient and volatile modifiers are only valid for member field declarations. abstract and native are
+ * only valid for member methods.  Note: a class declaration can have only final, abstract and public as modifiers,
+ * unless it is a nested class, in which case, it can be declared private orprotected as well. Within a method, a local variable may be declared as final. */
