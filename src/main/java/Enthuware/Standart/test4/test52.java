fe54e242c33f3b5a78245c3d94c0c5974544b41b
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-08 00:40:35 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test4/test52.java
@@ -0,0 +1,29 @@
+package Enthuware.Standart.test4;
+
+public class test52 {
+}
+/**What can be inserted in the following code so that it will print exactly 2345 when compiled and run?
+ *  public class FlowTest {      static int[] data = {1, 2, 3, 4, 5};
+ *  public static void main(String[] args) {
+ *  for (int i : data) {
+ *  if (i < 2) {
+ *  //insert code1 here
+ *  }
+ *  System.out.print(i);
+ *  if (i == 3) {
+ *  //insert code2 here
+ *  }
+ *  }
+ *  } }
+ *  */
+
+/**continue; and
+ * //nothing is required*/
+
+/**
+ continue;
+ and continue;*/
+
+/**This is a very simple loop to follow if you know what break and continue do.
+ break breaks the nearest outer loop. Once a break is encountered, no further iterations of that loop will execute.
+ continue simply starts the next iteration of the loop. Once a continue is encountered, rest of the statements within that loop are ignored (not executed ) and the next iteration is started.*/
