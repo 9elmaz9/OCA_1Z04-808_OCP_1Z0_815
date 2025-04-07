fc8eed14166e4c545ca08379b91fd4e81004175d
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-07 23:59:22 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test4/test47.java
@@ -0,0 +1,10 @@
+package Enthuware.Standart.test4;
+
+public class test47 {
+}
+/**What will be the output of the following code snippet?
+ *  int a = 1; int[] ia = new int[10];
+ *  int b = ia[a];
+ *  int c = b + a;
+ *  System.out.println(b = c);
+ * */
