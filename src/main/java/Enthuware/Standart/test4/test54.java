e5f7d6c36f0d5dd0cebed103fef7211420a20bc0
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-08 23:56:23 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test4/test54.java
@@ -0,0 +1,23 @@
+package Enthuware.Standart.test4;
+
+public class test54 {
+}
+/**What will be the result of attempting to compile and run the following program?
+
+ class TestClass{
+ public static void main(String args[]){
+ boolean b = false;
+ int i = 1;
+ do{
+ i++ ;
+ } while (b = !b);
+ System.out.println( i );
+ }
+ }*/
+
+
+//It will print 3.
+//The loop body is executed twice and the program will print 3.
+
+//Unlike the 'while(){}' loop, the 'do {} while()' loop executes at least once because the condition is checked after the iteration.
+
