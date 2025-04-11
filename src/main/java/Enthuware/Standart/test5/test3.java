3bb35fd5cd2d60778978b6f62927e8e082cf802f
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-12 00:00:35 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test5/test3.java
@@ -0,0 +1,24 @@
+package Enthuware.Standart.test5;
+
+public class test3 {
+}
+/*Which of the given lines can be inserted at //1 of the following program ?
+public class TestClass{
+   public static void main(String[] args){
+   short s = 9;
+     //1
+      }
+      }
+      */
+
+
+//    int i = 9; System.out.println(s == i);
+
+/*Any two numeric primitives can be compared using == operator. Even a numeric primitive and a primitive wrapper
+ (for example, an int with a Short or a Double) can be compared. However, two wrappers of different types
+ (for example, an Integer and a Short) cannot be compared using ==.*/
+
+
+
+//    Integer i = 9; System.out.println( s == i );
+
---
+++ src/main/java/Enthuware/Standart/test5/test4.java
@@ -0,0 +1,11 @@
+package Enthuware.Standart.test5;
+
+public class test4 {
+}
+/**The following code snippet will not compile:
+ *  int i = 10;
+ *  System.out.println( i<20 ? out1() : out2() );
+ *
+ * Assume that out1 and out2 methods have the following signatures: public void out1(); and public void out2();*/
+
+//True
