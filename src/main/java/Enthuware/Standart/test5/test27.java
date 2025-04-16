82eb4f171b99094f22be2ebe964006640e375ccd
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-17 00:00:40 +0200

    enthuware

--- src/main/java/Enthuware/Standart/test5/test27.java
+++ src/main/java/Enthuware/Standart/test5/test27.java
@@ -19,3 +19,14 @@ public class test27 {
 
 
 //    reverse( )
+
+
+//    setLength(int)
+/**Sets the length of the underlying string buffer. This string buffer is altered to represent a new character sequence whose length
+ *  is specified by the argument. For every nonnegative index k less than newLength, the character at index k in the new character sequence
+ *  is the same as the character at index k in the old sequence if k is less than the length of the old character sequence; otherwise,
+ *  it is the null character '' (\u0000). In other words, if the newLength argument is less than the current length of the string buffer,
+ *  the string buffer is truncated to contain exactly the number of characters given by the newLength argument. If the newLength argument
+ *  is greater than or equal to the current length, sufficient null characters ('\u0000') are appended to the string buffer so that length
+ *  becomes the newLength argument. The newLength argument must be greater than or equal to 0. Parameters: newLength - the new length of the buffer.
+ * Throws: IndexOutOfBoundsException - if the newLength argument is negative.*/
