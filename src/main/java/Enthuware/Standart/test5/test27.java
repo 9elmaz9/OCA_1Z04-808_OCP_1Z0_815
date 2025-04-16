bbd6a75e1108b150f5ada3c52e9a493ece92b4a3
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-17 00:00:13 +0200

    enthuware

--- src/main/java/Enthuware/Standart/test5/test27.java
+++ src/main/java/Enthuware/Standart/test5/test27.java
@@ -8,3 +8,14 @@ public class test27 {
 //This method is in String class.
 
 
+//yes
+//
+//ensureCapacity(int )
+//Ensures that the capacity of the buffer is at least equal to the specified minimum.
+
+
+//    append(boolean)
+//It has all sorts of overloaded append methods !!!
+
+
+//    reverse( )
