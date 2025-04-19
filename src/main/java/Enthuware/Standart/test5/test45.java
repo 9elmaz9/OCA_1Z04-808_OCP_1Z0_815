09329d04d609d29fa1e01a0684873fa26ccd3c26
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-19 23:36:56 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test5/test45.java
@@ -0,0 +1,17 @@
+package Enthuware.Standart.test5;
+
+public class test45 {
+}
+/**What would be the result of compiling and running the following program?
+ *  class SomeClass{
+ *  public static void main(String args[]){
+ *  int size = 10;
+ *  int[] arr = new int[size];
+ *  for (int i = 0 ; i < size ; ++i) System.out.println(arr[i]);
+ *  } }
+ *  */
+
+
+//The program will compile and run without error and will print 0 ten times.
+//It correctly will declare and initialize an array of length 10 containing int values initialized to have 0.
+
---
+++ src/main/java/Enthuware/Standart/test5/test46.java
@@ -0,0 +1,17 @@
+package Enthuware.Standart.test5;
+
+public class test46 {
+}
+/**What will the following program print?
+ public class TestClass{
+ public static void main(String[] args){
+ unsigned byte b = 0;
+ b--;
+ System.out.println(b);
+ }
+ }*/
+
+//It will not compile.
+
+//There no unsigned keyword in java! A char can be used as an unsigned integer.
+
