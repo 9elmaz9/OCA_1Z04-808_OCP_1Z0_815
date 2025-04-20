d897bbeb79482dfb78685f27f189b8d3939dcfb1
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-21 00:00:55 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test5/test52.java
@@ -0,0 +1,20 @@
+package Enthuware.Standart.test5;
+
+public class test52 {
+}
+/**Consider the following classes :
+
+ interface I{
+ }
+ class A implements I{
+ }
+
+ class B extends A {
+ }
+
+ class C extends B{
+ }
+ And the following declarations:
+ A a = new A();
+ B b = new B();
+ Identify options that will compile and run without error.*/
