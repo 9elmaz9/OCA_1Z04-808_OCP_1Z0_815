0624fcdad244ca745330d5a5143ffa2655f1c405
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-24 23:59:52 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test6/test4.java
@@ -0,0 +1,12 @@
+package Enthuware.Standart.test6;
+
+public class test4 {
+}
+/**Which of the following options correctly add 1 month and 1 day to a given LocalDate -
+ *  public LocalDate process(LocalDate ld){
+ *  //INSERT CODE HERE
+ *  return ld2; }*/
+
+
+//LocalDate ld2 = ld.plus(Period.of(0, 1, 1));
+/*public static Period of(int years, int months, int days) Obtains a Period representing a number of years, months and days. This creates an instance based on years, months and days.*/
