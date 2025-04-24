6c4151f574943d08b47932b71aebd40e5e0ad9da
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-25 00:00:20 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test6/test5.java
@@ -0,0 +1,13 @@
+package Enthuware.Standart.test6;
+
+public class test5 {
+}
+//    Which method declarations will enable a class to be run as a standalone program?
+
+
+//    final public static void main(String [ ] array)
+//final only means that subclasses cannot hide (in case of static methods) or override (in case of instance methods) that method.
+
+
+
+//    public static void main(String args[ ])
