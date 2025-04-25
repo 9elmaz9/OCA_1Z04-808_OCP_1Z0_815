9b7f6d2931b23c0b515ca6523d2422ffdc26f7d9
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-26 00:00:53 +0200

    enthuware

--- src/main/java/Enthuware/Standart/test6/test11.java
+++ src/main/java/Enthuware/Standart/test6/test11.java
@@ -2,10 +2,20 @@ package Enthuware.Standart.test6;
 
 public class test11 {
 }
-/**What is the result of executing the following fragment of code:  boolean b1 = false; boolean b2  = false; if (b2 = b1 != b2){    System.out.println("true"); } else{    System.out.println("false"); }
+/**What is the result of executing the following fragment of code:
+ *  boolean b1 = false;
+ *  boolean b2  = false;
+ *  if (b2 = b1 != b2){
+ *  System.out.println("true");
+ *  }
+ *  else{
+ *  System.out.println("false");
+ *  }
  * */
 
 
 //It will print false;
 
-/**All an if() needs is a boolean. Now, b1 != b2 returns false which is a boolean and so the expression becomes b2 = false.  It returns false which is again a boolean. So there is no error and it prints false. Remember that every expression has a return value (which is actually the Left Hand Side of the expression). For example, The value of the expressing i = 10 , is 10 (an int).*/
+/**All an if() needs is a boolean. Now, b1 != b2 returns false which is a boolean and so the expression becomes b2 = false.
+ * It returns false which is again a boolean. So there is no error and it prints false. Remember that every expression has a return value
+ * (which is actually the Left Hand Side of the expression). For example, The value of the expressing i = 10 , is 10 (an int).*/
