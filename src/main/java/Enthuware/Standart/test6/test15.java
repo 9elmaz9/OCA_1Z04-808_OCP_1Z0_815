d1ba64d3c9f625ef0d2c48491c9dafd46a12661d
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-27 23:24:02 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test6/test15.java
@@ -0,0 +1,17 @@
+package Enthuware.Standart.test6;
+
+public class test15 {
+}
+//    What is meant by "encapsulation" ?
+
+//    Member fields are declared private and public accessor/mutator methods are provided to access and change their values if needed.
+
+/**Encapsulation is one of the 4 fundamentals of OOP (Object Oriented Programming).
+
+ Encapsulation means that the internal representation of an object is generally hidden from view outside of the object's definition.
+ Some languages like Smalltalk and Ruby only allow access via object methods, but most others (e.g. C++ or Java) offer the programmer a
+ degree of control over what is hidden, typically via keywords like public and private.
+
+ Hiding the internals of the object protects its integrity by preventing users from setting the internal data of the component into
+ an invalid or inconsistent state. A benefit of encapsulation is that it can reduce system complexity, and thus increases robustness,
+ by allowing the developer to limit the interdependencies between software components.*/
