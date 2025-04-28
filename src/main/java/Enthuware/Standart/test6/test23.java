5a147c457c4f4dc7e341f5b7d4b7955c8e39d401
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-04-29 00:02:19 +0200

    update

---
+++ src/main/java/Enthuware/Standart/test6/test23.java
@@ -0,0 +1,10 @@
+package Enthuware.Standart.test6;
+
+public class test23 {
+}
+//    Which of the following is illegal ?
+
+
+//    byte b = 320;
+//320 cannot fit into a byte so you must cast it.: byte b = (byte) 320;
+
---
+++ src/main/java/Enthuware/Standart/test6/test24.java
@@ -0,0 +1,15 @@
+package Enthuware.Standart.test6;
+
+public class test24 {
+}
+/**What will the following lines of code print
+ *  java.time.LocalDate dt = java.time.LocalDate.parse("2015-01-01")
+ *  .minusMonths(1).minusDays(1).plusYears(1);
+ *  System.out.println(dt);*/
+
+//2015-11-30
+//The numbering for days and months starts with 1. Rest is simple math.
+
+/**Observe that most of the methods of LocalDate (as well as LocalTime and LocalDateTime) return an object of the same class.
+ * This allows you to chain the calls as done in this question. However, these  methods return a new object.
+ * They don't modify the object on which the method is called. */
