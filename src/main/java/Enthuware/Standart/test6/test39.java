ca9037328f464ee412fc725c9936b773273ffb45
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-05-04 23:57:47 +0200

    update

---
+++ src/main/java/Enthuware/Standart/test6/test39.java
@@ -0,0 +1,21 @@
+package Enthuware.Standart.test6;
+
+public class test39 {
+}
+/**What would be the result of trying to compile and run the following program?
+ * public class Test{
+ * int[] ia = new int[1];
+ * Object oA[]  = new Object[1];
+ *  boolean bool;
+ *  public static void main(String args[]){
+ *  Test test = new Test();
+ *  System.out.println(test.ia[0] + "  " + test.oA[0]+"  "+test.bool);
+ *  } } */
+
+//The program will print "0 null false".
+
+/**Following are the default values that instance variables are initialized with if not initialized explicitly:
+ types (byte, short, char, int, long, float, double) to 0 ( or 0.0 ).
+ All Object types to null.
+ boolean to false.*/
+
