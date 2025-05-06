94ec1427e9a3f2d170320e5d818b799130538a0f
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-05-07 00:00:18 +0200

    update

---
+++ src/main/java/Enthuware/Standart/test6/test42.java
@@ -0,0 +1,12 @@
+package Enthuware.Standart.test6;
+
+public class test42 {
+}
+/**What will the following code print?
+ *  String s = "blooper";
+ *  StringBuilder sb = new StringBuilder(s);
+ *  sb.append(s.substring(4)).delete(3, 5);
+ *  System.out.println(sb);*/
+
+//bloerper
+
