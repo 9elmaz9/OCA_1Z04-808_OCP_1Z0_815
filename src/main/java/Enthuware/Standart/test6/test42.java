3e3880793ddc3a708042c01bc520ac54b30bd343
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-05-07 00:01:03 +0200

    update

--- src/main/java/Enthuware/Standart/test6/test42.java
+++ src/main/java/Enthuware/Standart/test6/test42.java
@@ -10,4 +10,10 @@ public class test42 {
 
 //bloerper
 
-/**Please read the following description of substring method of String and delete method of StringBuilder:  public String substring(int beginIndex)   Returns a new string that is a substring of this string. The substring begins with the character at the specified index and extends to the end of this string. Examples: "unhappy".substring(2) returns "happy" "Harbison".substring(3) returns "bison" "emptiness".substring(9) returns "" (an empty string)   public StringBuilder delete(int start, int end)   Removes the characters in a substring of this sequence. The substring begins at the specified start and extends to the character at index end - 1 or to the end of the sequence if no such character exists. If start is equal to end, no changes are made. */
+/**Please read the following description of substring method of String and delete method of StringBuilder:
+ * public String substring(int beginIndex) Returns a new string that is a substring of this string.
+ * The substring begins with the character at the specified index and extends to the end of this string.
+ * Examples: "unhappy".substring(2) returns "happy" "Harbison".substring(3) returns "bison" "emptiness".substring(9) returns "" (an empty string)
+ * StringBuilder delete(int start, int end) Removes the characters in a substring of this sequence.
+ * The substring begins at the specified start and extends
+ * to the character at index end - 1 or to the end of the sequence if no such character exists. If start is equal to end, no changes are made. */
