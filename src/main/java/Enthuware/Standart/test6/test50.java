49e3f40d03f77cb98e565d54b434a6fa48ff7eac
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-05-09 00:06:26 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test6/test50.java
@@ -0,0 +1,23 @@
+package Enthuware.Standart.test6;
+
+public class test50 {
+}
+/**Which of these for statements are valid?
+
+ 1. for (int i=5; i=0; i--) { }
+
+ 2.  int j=5;
+ for(int i=0, j+=5;  i<j ; i++) {  j--;  }
+
+ 3. int i, j;
+ for (j=10; i<j; j--) { i += 2; }
+
+ 4. int i=10;
+ for ( ; i>0 ; i--) { }
+
+ 5. for (int i=0, j=10; i<j; i++, --j) {;}*/
+
+//4, 5
+
+/*No 1.
+uses '=' instead of '==' for condition which is invalid. The loop condition must be of type boolean.*/
