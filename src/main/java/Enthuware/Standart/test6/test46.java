698fda91ae9c11555b3d9784c9bcf2210d623ce0
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-05-09 00:00:34 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test6/test46.java
@@ -0,0 +1,16 @@
+package Enthuware.Standart.test6;
+
+public class test46 {
+}
+/**What will be the output if you run the following program?
+
+ public class TestClass{
+ public static void main(String args[]){
+ int i;
+ int j;
+ for (i = 0, j = 0 ; j < 1 ; ++j , i++){
+ System.out.println( i + " " + j );
+ }
+ System.out.println( i + " " + j );
+ }
+ }*/
