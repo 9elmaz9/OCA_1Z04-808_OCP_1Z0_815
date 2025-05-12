7253c69a2e9f089582d933db96e46fe4be2a645a
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-05-13 00:06:10 +0200

    update

--- src/main/java/Enthuware/Standart/test7/test1.java
+++ src/main/java/Enthuware/Standart/test7/test1.java
@@ -19,3 +19,21 @@ public class test1 {
  *  al.add(new Data(1));al.add(new Data(2));al.add(new Data(3));
  *  //INSERT METHOD CALL HERE
  *  Which of the following options can be inserted above so that it will print 1 4 9?*/
+
+
+//processList(al, (int a, int b)->System.out.println(a*b) );
+
+
+
+//processList(al, (a, b)->System.out.println(a*b));
+//It is ok to omit the parameter types in case of a functional interface because
+// the compiler can determine the type of the parameters by looking at the interface method.
+
+
+
+//processList(al, (a, b) ->{  System.out.println(a*b); } );
+//If you enclose your method body within curly braces, you must write complete lines of code
+// including the semi-colon. FYI, if the method is supposed to return a value, then you must include
+// a return statement just like you do in a regular method if you are using the curly braces syntax.
+
+
