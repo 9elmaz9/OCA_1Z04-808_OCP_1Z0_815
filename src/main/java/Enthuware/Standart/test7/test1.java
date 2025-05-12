7c75a0ebbe71f8924aaba99946fac02621fa2b7d
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-05-13 00:08:41 +0200

    update

--- src/main/java/Enthuware/Standart/test7/test1.java
+++ src/main/java/Enthuware/Standart/test7/test1.java
@@ -31,9 +31,20 @@ public class test1 {
 
 
 
-//processList(al, (a, b) ->{  System.out.println(a*b); } );
+//processList(al, (a, b) ->{System.out.println(a*b); } );
 //If you enclose your method body within curly braces, you must write complete lines of code
 // including the semi-colon. FYI, if the method is supposed to return a value, then you must include
 // a return statement just like you do in a regular method if you are using the curly braces syntax.
+
+/**There is a simple trick to identify invalid lambda constructs.
+ *  When you write a lambda expression for a functional interface, you are essentially
+ *  providing an implementation of the method declared in that interface but in a very concise manner.
+ *  Therefore, the lambda expression code that you write must contain all the pieces of the regular
+ *  method code except the ones that the compiler can easily figure out on its own such as the parameter types,
+ *  return keyword, and brackets. So, in a lambda expression, just check that all the information is there
+ *  and that the expression follows the basic syntax -  (parameter list) OR
+ *  single_variable_without_type -> { regular lines of code } OR
+ *  For a complete discussion on this topic please see this short tutorial - https://enthuware.com/lambda-for-ocajp*/
+
 
 
