02717349a1e9967057a6e6f869a548d69dadd91a
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-05-17 23:03:57 +0200

    update

---
+++ src/main/java/Enthuware/Standart/test7/test8.java
@@ -0,0 +1,26 @@
+package Enthuware.Standart.test7;
+
+public class test8 {
+}
+/**What will the following code print when run?
+
+ class A{
+ String value = "test";
+ A(String val){
+ this.value = val;
+ }
+ }
+ public class TestClass {
+ public static void main(String[] args) throws Exception {
+ new A("new test").print();
+ }
+ }*/
+
+//It will not compile.
+
+
+/**There is no method named print() defined in class A.
+ *  Further, there is no such method in class Object either.
+ *  To print the contents of an object you can use toString() method that returns a String:
+ *  System.out.println(a.toString());  However, for this to print a meaningful value,
+ *  class A should override the Object class's toString() method to return a meaningful String.*/
