048b30a55109b18eb23a7d04d8db44585e3def80
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-05-29 00:00:21 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test7/test26.java
@@ -0,0 +1,14 @@
+package Enthuware.Standart.test7;
+
+public class test26 {
+}
+/**Identify correct statement(s) about the following code:
+ *  int value = 1,000,000; //1
+ *  switch(value){
+ *  case 1_000_000 : System.out.println("A million 1"); //2
+ *  break;
+ *  case 1000000 : System.out.println("A million 2"); //3
+ *  break;
+ *   }*/
+
+/*Compilation fails at line //1 and //3*/
