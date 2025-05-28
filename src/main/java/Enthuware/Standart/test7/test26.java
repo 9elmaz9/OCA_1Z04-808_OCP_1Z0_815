8202abe61124f433ce876f12c20dd3dd4af420a2
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-05-29 00:01:02 +0200

    enthuware

--- src/main/java/Enthuware/Standart/test7/test26.java
+++ src/main/java/Enthuware/Standart/test7/test26.java
@@ -12,3 +12,11 @@ public class test26 {
  *   }*/
 
 /*Compilation fails at line //1 and //3*/
+
+/***1. You may use underscores (but not commas) to format a number for better code readability.
+ *  So //1 is invalid. 2. Adding underscores doesn't actually change the number.
+ *  The compiler ignores the underscores. So 1_000_000 and 1000000 are actually same and you cannot
+ *  have two case blocks with the same value. Therefore, the second case at //3 is invalid.
+ *  You may use underscore for all kinds of numbers including long, double, float, binary, as well as hex.
+ *  For example, the following are all valid numbers - int hex = 0xCAFE_BABE;
+ *  float f = 9898_7878.333_333f; int bin = 0b1111_0000_1100_1100;*/
