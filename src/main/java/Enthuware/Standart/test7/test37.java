0a2fd3e2238a6449da04e417a285e21f2345f96c
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-06-16 23:58:14 +0200

    update

---
+++ src/main/java/Enthuware/Standart/test7/test37.java
@@ -0,0 +1,12 @@
+package Enthuware.Standart.test7;
+
+public class test37 {
+}
+/**Given:  StringBuilder sb = new StringBuilder("asdf");
+ *  Which of the following code fragments will print true?*/
+
+/**String str1 = sb.toString();
+ * String str2 = str1;
+ * System.out.println(str1 == str2);*/
+
+//Since str1 and str2 both point to the same String object, == will return true.
