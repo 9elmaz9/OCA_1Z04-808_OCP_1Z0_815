dbe5b26095344dd8a257abcaaf26d8e4b1c77e5f
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-06-17 00:06:23 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test7/test44.java
@@ -0,0 +1,13 @@
+package Enthuware.Standart.test7;
+
+public class test44 {
+}
+/**What will the following code print when compiled and run?
+ *  int [] [] array = {{0}, {0, 1}, {0, 1, 2}, {0, 1, 2, 3}, {0, 1, 2, 3, 4}};
+ *  int[] arr1 = array[4];
+ *  System.out.println (arr1[4][1]);
+ *  System.out.println (array[4][1]);*/
+
+//It will not compile.
+//arr1 is an array of one dimension. But arr1[4][1] is trying to access it as a two dimensional array. This will, therefore, not compile.
+
