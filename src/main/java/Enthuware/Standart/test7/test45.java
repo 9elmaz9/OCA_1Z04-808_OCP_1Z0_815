988f5e6e5e18a1ead16632678ea2185798cd6dc9
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-06-17 00:06:45 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test7/test45.java
@@ -0,0 +1,10 @@
+package Enthuware.Standart.test7;
+
+public class test45 {
+}
+/**You are writing a piece of code that determines tax rate on a given grossIncome. The tax rate is to be computed as follows -
+ If grossIncome is less than or equals to 18000, taxRate is 0.
+ If grossIncome is more than 18000 but less than or equal to 36000, taxRate is 10%
+ If grossIncome is more than 36000, taxRate is 20%.
+
+ Which of following code fragments do it correctly?*/
