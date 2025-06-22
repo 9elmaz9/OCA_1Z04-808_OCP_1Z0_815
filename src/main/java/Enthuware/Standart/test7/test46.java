5d7d11eeb81c043bf516c6b3d5dcb6c61d0684a3
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-06-22 23:57:50 +0200

    enthuware

---
+++ src/main/java/Enthuware/Standart/test7/test46.java
@@ -0,0 +1,26 @@
+package Enthuware.Standart.test7;
+
+public class test46 {
+}
+/**
+ * What can be inserted in the code below so that it will print UP UP UP?
+ * public class Speak {
+ * public static void main(String[] args) {
+ * Speak s = new GoodSpeak();
+ * INSERT CODE HERE
+ *   }
+ *  }
+ * class GoodSpeak extends Speak implements Tone{
+ * public void up(){
+ * System.out.println("UP UP UP");
+ *   }
+ *  }
+ * interface Tone{
+ * void up();
+ *  }*/
+
+
+//((Tone)s).up();
+
+
+//((GoodSpeak)s).up();
