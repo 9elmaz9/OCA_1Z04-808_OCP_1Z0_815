74b55369a8fa9340b9335d43e8f0528c8e487532
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-07-06 00:04:06 +0200

    update

--- src/main/java/Enthuware/Standart/eight/Test13.java
+++ src/main/java/Enthuware/Standart/eight/Test13.java
@@ -4,6 +4,50 @@ package Enthuware.Standart.eight;
 public class Test13  {
 
 }
+/**What will the following code print when compiled and run?
+ *  public class TestClass{    public static void main(String[] args){
+ *  int[] arr = { 1, 2, 3, 4, 5, 6 };         int counter = 0;
+ *  for (int value : arr) {
+ *  if (counter >= 5) {
+ *  break;
+ *   } else {
+ *  continue;
+ *   }
+ *  if (value > 4) {
+ *  arr[counter] = value + 1;
+ *   }
+ *  counter++;
+ *    }
+ *  System.out.println(arr[counter]);
+ *     }
+ *   }*/
+
+
+
+
+//It will not compile.
+
+//Observe that the lineif (value > 4) { and the rest of the code in the for loop will not execute in any case. It is therefore unreachable code and the compiler will complain about it.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  *
  *
