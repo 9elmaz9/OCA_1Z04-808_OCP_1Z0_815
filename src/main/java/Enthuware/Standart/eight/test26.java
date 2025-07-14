a7db2f6e3edce86d6fc5cecda5a1f3ff6cdec175
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-07-14 23:59:04 +0200

    update

---
+++ src/main/java/Enthuware/Standart/eight/test26.java
@@ -0,0 +1,21 @@
+package Enthuware.Standart.eight;
+
+public class test26 {
+}
+/**Given: public class CrazyMath {
+ *  public static void main(String[] args) {
+ *  int x = 10, y = 20;
+ *  int dx, dy;
+ *  try{
+ *  dx = x % 5;
+ *  dy =  y/dx;
+ *  }catch(ArithmeticException ae){
+ *  System.out.println("Caught AE");
+ *  dx = 2;
+ *  dy = y/dx;
+ *  }
+ *  x = x/dx;
+ *  y = y/dy;
+ *  System.out.println(dx+" "+dy);
+ *  System.out.println(x+" "+y);
+ *  } } What is the output?*/
