b4b6468f6638dcc26674907588bf4181afaf075e
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-07-15 00:01:07 +0200

    update

---
+++ src/main/java/Enthuware/Standart/eight/test27.java
@@ -0,0 +1,10 @@
+package Enthuware.Standart.eight;
+
+public class test27 {
+}
+
+//    Which of the following are benefits of ArrayList over an array?
+
+//You do not have to worry about the size of the ArrayList while appending elements.
+/**An ArrayList resized dynamically at run time as per the situation. An array cannot be resized once created.
+ * This reduces the amount of boiler plate code that is required to do the same task using an array.*/
