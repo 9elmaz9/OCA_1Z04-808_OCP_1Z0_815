ede6592196eef43b6a9a791e2773420013d6f9d5
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-07-14 23:56:52 +0200

    update

---
+++ src/main/java/Enthuware/Standart/eight/test25.java
@@ -0,0 +1,8 @@
+package Enthuware.Standart.eight;
+
+public class test25 {
+}
+/**Given the following pairs of method declarations, which of the statements are true?
+ *  1. void perform_work(int time){ } int  perform_work(int time, int speed){ return time*speed ;}
+ *  2. void perform_work(int time){ } int  perform_work(int speed){return speed ;}
+ *  3. void perform_work(int time){ } void Perform_work(int time){ }*/
