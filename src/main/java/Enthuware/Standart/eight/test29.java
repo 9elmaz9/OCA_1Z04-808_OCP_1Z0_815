99519097214e967b01fc7eb9ae04c5e993d137e3
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-07-19 23:50:11 +0200

    update

---
+++ src/main/java/Enthuware/Standart/eight/test29.java
@@ -0,0 +1,12 @@
+package Enthuware.Standart.eight;
+
+public class test29 {
+}
+/**Consider the following lines of code:
+ * System.out.println(null + true); //1
+ * System.out.println(true + null); //2
+ * System.out.println(null + null); //3
+ *
+ * Which of the following statements are correct?*/
+
+
