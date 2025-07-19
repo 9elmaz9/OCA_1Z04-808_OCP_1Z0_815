cad532093ed685a1b85365dd2ce76ef563f7c2c6
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-07-19 23:54:00 +0200

    update

---
+++ src/main/java/Enthuware/Standart/eight/test31.java
@@ -0,0 +1,73 @@
+package Enthuware.Standart.eight;
+
+public class test31 {
+}
+/**Given the class
+ // Filename: Test.java
+ public class Test{
+ public static void main(String args[]){
+ for(int i = 0; i< args.length; i++){
+ System.out.print("  "+args[i]);
+ }
+ }
+ }
+ Now consider the following 3 options for running the program:
+ a: java Test
+ b: java Test param1
+ c: java Test param1 param2
+ Which of the following statements are true?*/
+
+
+//It will print param1 param2 on option c.
+
+
+//It will not print anything on option a.
+
+//It will not throw NullPointerException because args[] is never null. If no argument is given (as in option a) then the length of args is 0.
+
+/**It will not throw NullPointerException because args[] is never null. If no argument is given (as in option a) then the length of args is 0.
+
+
+ Давайте разберемся по порядку, чтобы понять, почему правильные ответы такие.
+
+ Код:
+ java
+ Copy code
+ public class Test {
+ public static void main(String args[]) {
+ for (int i = 0; i < args.length; i++) {
+ System.out.print(" " + args[i]);
+ }
+ }
+ }
+ Этот код пытается пройти по массиву args (который содержит аргументы командной строки) и вывести каждый аргумент. Но важно учитывать, что массив args может быть пустым, если программа запускается без аргументов.
+
+ Разбор каждого из вариантов:
+ 1. java Test (Без аргументов):
+ Когда программа запускается с пустым массивом args (без аргументов), то args.length равно 0.
+ Цикл for (int i = 0; i < args.length; i++) не будет выполняться, так как условие i < args.length не выполнится (поскольку args.length равно 0).
+ Вывод: Программа не выведет ничего, так как цикл не будет выполняться.
+ Правильный вывод: Программа не выведет ничего на экране в этом случае. Поэтому утверждение "It will not print anything on option a." верно.
+
+ 2. java Test param1 (Один аргумент):
+ Когда программа запускается с одним аргументом (param1), массив args будет содержать один элемент: args = ["param1"].
+ Цикл for (int i = 0; i < args.length; i++) выполнится один раз, и на первой итерации args[i] будет равно "param1".
+ Вывод: Программа выведет param1 на экран.
+ Правильный вывод: Программа выведет param1 на экране. Поэтому утверждение "The program will print param1 on option b." верно.
+
+ 3. java Test param1 param2 (Два аргумента):
+ Когда программа запускается с двумя аргументами (param1 и param2), массив args будет содержать два элемента: args = ["param1", "param2"].
+ Цикл for (int i = 0; i < args.length; i++) выполнится дважды:
+ В первой итерации args[0] = "param1".
+ Во второй итерации args[1] = "param2".
+ Вывод: Программа выведет param1 param2 на экран.
+ Правильный вывод: Программа выведет param1 param2 на экране. Поэтому утверждение "It will print param1 param2 on option c." верно.
+
+ Разбор неправильных утверждений:
+ The program will throw java.lang.ArrayIndexOutOfBoundsException on option a.:
+ Это утверждение неверно, потому что программа не выйдет за пределы массива. Цикл не будет выполнен, когда нет аргументов (если args.length == 0).
+ The program will throw java.lang.NullPointerException on option a.:
+ Это утверждение неверно, потому что массив args никогда не будет null — он будет просто пустым, если не передаются аргументы.
+ Ответ:
+ "The program will print param1 on option b."
+ "It will not print anything on option a."*/
