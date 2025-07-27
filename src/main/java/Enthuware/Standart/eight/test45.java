5c4962973d609f4eab9c9c316fcd25037fd3656a
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-07-27 22:33:31 +0200

    update

---
+++ src/main/java/Enthuware/Standart/eight/test45.java
@@ -0,0 +1,22 @@
+package Enthuware.Standart.eight;
+
+public class test45 {
+}
+/**Consider the following code:
+
+ class Super{
+ static{ System.out.print("super "); }
+ }
+ class One{
+ static {  System.out.print("one "); }
+ }
+ class Two extends Super{
+ static {  System.out.print("two "); }
+ }
+ class Test{
+ public static void main(String[] args){
+ One o = null;
+ Two t = new Two();
+ }
+ }
+ What will be the output when class Test is run ?*/
