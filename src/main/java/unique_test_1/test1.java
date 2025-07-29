62b841f9c67bdd6bba228bbc5c564d0adb663ded
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-07-29 22:20:00 +0200

    update

--- src/main/java/unique_test_1/test1.java
+++ src/main/java/unique_test_1/test1.java
@@ -26,3 +26,14 @@ public class test1 {
  Exception in thread "main" java.lang.Exception: Too high!
  at TestClass.doStuff(TestClass.java:29)
  at TestClass.main(TestClass.java:41)*/
+
+/**There are only two possibilities:
+ *  1. If Math.random() generates a number more than 0.4, the if part will throw an exception.
+ *  In this case, the remain code of doStuff will not be called and main() will receive an exception due
+ *  to the call to doStuff. Since doStuff() is not within a try/catch block, the exception will propagate
+ *  up and the remaining code in main() will not be executed either.  Since the exception is not caught
+ *  anywhere in the code, it will finally reach the JVM's thread that has called the main method.
+ *  This thread catches the exception and prints out the stack trace.
+ *
+ *  2. If Math.random() generates a number not more than 0.4, if part will not be executed
+ *  and "Done stuff." will be printed. After the call returns in main(), "Over" will be printed as well.*/
