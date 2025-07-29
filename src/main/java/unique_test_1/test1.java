ecac7d856cf2684968f6c22afab0bfd07cb9535f
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-07-29 22:19:15 +0200

    update

--- src/main/java/unique_test_1/test1.java
+++ src/main/java/unique_test_1/test1.java
@@ -20,3 +20,9 @@ public class test1 {
  * Which of the following are possible outputs when the above program is compiled and run?
  * (Assume that Math.random() returns a double between 0.0 and 1.0 not including 1.0.
  * Further assume that there is no mistake in the line numbers printed in the output shown in the options.)*/
+
+
+/**Doing stuff...
+ Exception in thread "main" java.lang.Exception: Too high!
+ at TestClass.doStuff(TestClass.java:29)
+ at TestClass.main(TestClass.java:41)*/
