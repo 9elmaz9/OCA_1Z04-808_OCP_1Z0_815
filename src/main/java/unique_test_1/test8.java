62416fe0b67321057e5e3ff8c2ac4114e3e66792
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-03 23:35:04 +0200

    update

---
+++ src/main/java/unique_test_1/test8.java
@@ -0,0 +1,19 @@
+package unique_test_1;
+
+public class test8 {
+}
+
+/**What will the following program print?
+
+ public class TestClass{
+ static String str;
+ public static void main(String[] args){
+ System.out.println(str);
+ }
+ }*/
+
+//It will print 'null' (without quotes).
+
+
+/**All member fields (static and non-static) are initialized to their default values.
+ * Objects are initialized to null (String is also an object), numeric types to 0 (or 0.0 ) and boolean to false.*/
