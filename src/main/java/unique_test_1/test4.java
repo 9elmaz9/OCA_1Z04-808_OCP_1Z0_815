6359709fa4ad0a91ee43e85dbfde2f0527d6c9b3
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-03 23:30:17 +0200

    update

---
+++ src/main/java/unique_test_1/test4.java
@@ -0,0 +1,12 @@
+package unique_test_1;
+
+public class test4 {
+}
+/*Which of the following lambda expressions can be used to invoke a method
+ that accepts a java.util.function.Predicate as an argument?*/
+
+
+//x -> x == null
+
+
+//x->true
