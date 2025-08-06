8374ed1d6f71d29da9dc129856410ebf41c543ca
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-06 22:12:53 +0200

    update

---
+++ src/main/java/unique_test_1/test12.java
@@ -0,0 +1,21 @@
+package unique_test_1;
+
+public class test12 {
+}
+/**Given:
+ *  public class TableTest {
+ *  static String[][] table;
+ *  public static void main(String[] args) {
+ *  String[] x = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+ *  String[] y1 = { "1", "2", "3", "4", "5" };
+ *  String[] y2 = { "a", "b", "c" };
+ *  table = new String[3][];
+ *  table[0] = x;
+ *  table[1] = y1;
+ *  table[2] = y2;          //INSERT CODE HERE
+ *  } }
+ *  What can be inserted in the above code to make it print Sun5c?*/
+
+/**for(String[] row : table){
+ * System.out.print(row[row.length-1]); }*/
+
