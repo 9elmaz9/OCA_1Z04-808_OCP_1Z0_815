288be2481b70cf545662b999ec13ab2900ec813d
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-09 23:42:18 +0200

    update

---
+++ src/main/java/unique_test_1/test16.java
@@ -0,0 +1,10 @@
+package unique_test_1;
+
+public class test16 {
+}
+/**Which of the following are valid ways to create a LocalDateTime?*/
+
+
+/**
+ java.time.LocalDateTime.of(2015, 10, 1, 10, 10);*/
+
