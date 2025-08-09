44f0ec8c3effa5d20236c197d34c068793573a8c
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-09 23:45:01 +0200

    update

---
+++ src/main/java/unique_test_1/test18.java
@@ -0,0 +1,24 @@
+package unique_test_1;
+
+public class test18 {
+}
+
+/**Which of the statements regarding the following code are correct?
+ *  public class TestClass{
+ *  static int a;
+ *  int b;
+ *  public TestClass(){
+ *  int c;
+ *  c = a;
+ *  a++;
+ *  b += c;
+ *  }
+ *  public static void main(String args[]) {
+ *  new TestClass();
+ *  } }*/
+
+//The code will compile and run without any problem.
+
+//All the instance or static variables are given a default values if not explicitly initialized. All numeric variable are given a value of zero or equivalent to zero (i.e. 0.0 for double or float ).
+//booleans are initialized to false and objects references are initialized to null.
+
