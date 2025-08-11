44d11876fba64e97e7acb394d6dd4f2af6eba5c1
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-12 00:00:22 +0200

    update

---
+++ src/main/java/unique_test_1/test23.java
@@ -0,0 +1,9 @@
+package unique_test_1;
+
+public class test23 {
+}
+/**Which statements about the following code are correct?
+ * interface House{
+ * public default String getAddress(){
+ * return "101 Main Str";   } }  interface Bungalow extends House{   public default String getAddress(){      return "101 Smart Str";   } }  class MyHouse implements Bungalow, House{  }  public class TestClass {    public static void main(String[] args) {
+ *  House ci = new MyHouse();  //1     System.out.println(ci.getAddress()); //2   } } */
