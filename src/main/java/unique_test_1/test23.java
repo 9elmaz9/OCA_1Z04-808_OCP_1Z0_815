3e8bf2d609a150cb99ccfd1e776e2f93151950a3
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-12 00:00:46 +0200

    update

--- src/main/java/unique_test_1/test23.java
+++ src/main/java/unique_test_1/test23.java
@@ -5,5 +5,12 @@ public class test23 {
 /**Which statements about the following code are correct?
  * interface House{
  * public default String getAddress(){
- * return "101 Main Str";   } }  interface Bungalow extends House{   public default String getAddress(){      return "101 Smart Str";   } }  class MyHouse implements Bungalow, House{  }  public class TestClass {    public static void main(String[] args) {
- *  House ci = new MyHouse();  //1     System.out.println(ci.getAddress()); //2   } } */
+ * return "101 Main Str";   } }
+ * interface Bungalow extends House{
+ * public default String getAddress(){
+ * return "101 Smart Str";   } }
+ * class MyHouse implements Bungalow, House{  }
+ * public class TestClass {
+ * public static void main(String[] args) {
+ *  House ci = new MyHouse();  //1
+ *  System.out.println(ci.getAddress()); //2   } } */
