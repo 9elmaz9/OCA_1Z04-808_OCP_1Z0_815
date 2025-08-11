efaa3a600d27d52188a8e1ceee738dce7208caa6
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-12 00:07:19 +0200

    update

--- src/main/java/unique_test_1/test23.java
+++ src/main/java/unique_test_1/test23.java
@@ -5,12 +5,30 @@ public class test23 {
 /**Which statements about the following code are correct?
  * interface House{
  * public default String getAddress(){
- * return "101 Main Str";   } }
+ * return "101 Main Str";
+ *   }
+ *  }
  * interface Bungalow extends House{
  * public default String getAddress(){
- * return "101 Smart Str";   } }
+ * return "101 Smart Str";
+ *   }
+ * }
  * class MyHouse implements Bungalow, House{  }
  * public class TestClass {
  * public static void main(String[] args) {
  *  House ci = new MyHouse();  //1
- *  System.out.println(ci.getAddress()); //2   } } */
+ *  System.out.println(ci.getAddress()); //2
+ *    }
+ *  } */
+
+//The code will compile successfully.
+
+/**There is no problem with the code. It is perfectly valid for a subinterface
+ * to override a default method of the base interface. A class that implements
+ * an interface can also override a default method. It is valid for MyHouse
+ * to say that it implements Bungalow as well as House even though House is
+ * redundant because Bungalow is a House anyway.  It will actually print 101 Smart str.*/
+
+
+
+
