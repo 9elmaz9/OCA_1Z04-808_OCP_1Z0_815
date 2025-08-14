62e1dcff32302001cb1db7c3f315d1b6297da2e7
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-14 22:29:30 +0200

    update

---
+++ src/main/java/unique_test_1/test24.java
@@ -0,0 +1,13 @@
+package unique_test_1;
+
+public class test24 {
+}
+/**Which of the following are features of Java?
+ Some candidates have reported a similar question being asked with a slightly different (and ambiguous) wording -
+ Which of the following are objected oriented features of Java?*/
+
+//Object is the root class of all the classes.
+
+//Objects can share behavior with other objects.
+
+//Another very vague statement but it is probably true because two objects of two different classes may share some behavior if the two classes inherit from the same super class.
