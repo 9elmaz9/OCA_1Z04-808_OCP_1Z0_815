2b155a9c145d2d933cfaebe7bc4009290d4a8c24
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-14 22:31:20 +0200

    update

---
+++ src/main/java/unique_test_1/test26.java
@@ -0,0 +1,18 @@
+package unique_test_1;
+
+public class test26 {
+}
+/**Consider the following class...
+ *  class TestClass{
+ *  int i;
+ *  public TestClass(int i) { this.i = i;  }
+ *  public String toString(){
+ *  if(i == 0) return null;
+ *  else return ""+i;    }
+ *  public static void main(String[ ] args){
+ *  TestClass t1 = new TestClass(0);
+ *  TestClass t2 = new TestClass(2);
+ *  System.out.println(t2);
+ *  System.out.println(""+t1);
+ *  } }
+ *  What will be the output when the above program is run?*/
