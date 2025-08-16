543feee48c08e58947c7f735182436707dc2fb25
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-17 00:00:39 +0200

    update

---
+++ src/main/java/unique_test_1/test29.java
@@ -0,0 +1,12 @@
+package unique_test_1;
+
+public class test29 {
+}
+/**Consider the following class:
+ *  public class PortConnector{
+ *  public PortConnector(int port) throws IOException{
+ *  ...lot of valid code.    }
+ *  ...other valid code. }
+ *
+ *  You want to write another class CleanConnector that extends from PortConnector.
+ * Which of the following statements should hold true for CleanConnector class?*/
