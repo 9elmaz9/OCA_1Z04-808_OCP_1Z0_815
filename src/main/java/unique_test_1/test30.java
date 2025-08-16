bea82db8e04dc0b815452d80be95a3620ed700e9
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-08-17 00:02:18 +0200

    update

---
+++ src/main/java/unique_test_1/test30.java
@@ -0,0 +1,28 @@
+package unique_test_1;
+
+public class test30 {
+}
+/**Consider the following two classes (in the same package but defined in different source files):
+
+ public class Square {
+ double side = 0;
+ double area;
+
+ public Square(double length){        this.side = length;    }
+
+ public double getSide() {  return side;    }
+
+ public void setSide(double side) {  this.side = side;   }
+
+ double getArea() {   return area;   }
+ }
+ public class TestClass {
+ public static void main(String[] args) throws Exception {
+ Square sq = new Square(10.0);
+ sq.area = sq.getSide()*sq.getSide();
+ System.out.println(sq.getArea());
+ }
+ }
+ You are assigned the task of refactoring the Square class to make it better in terms of encapsulation.
+ What changes will you make to this class?*/
+
