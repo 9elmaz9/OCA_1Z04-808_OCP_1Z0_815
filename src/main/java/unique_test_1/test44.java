48302b2d6308fb3d455fb4921f2822ed7a6f623d
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-09-02 00:05:38 +0200

    update

---
+++ src/main/java/unique_test_1/test44.java
@@ -0,0 +1,47 @@
+package unique_test_1;
+
+public class test44 {
+}
+/**Consider the following two classes (in the same package but defined in different source files):
+ public class Square {
+ double side = 0;
+ double area;
+
+ public Square(double length){        this.side = length;    }
+
+ public double getSide() {  return side;    }
+
+ public void setSide(double side) {  this.side = side;   }
+
+ double getArea() {   return area;   }
+ }
+ public class TestClass {
+ public static void main(String[] args) throws Exception {
+ Square sq = new Square(10.0);
+ sq.area = sq.getSide()*sq.getSide();
+ System.out.println(sq.getArea());
+ }
+ }
+ You are assigned the task of refactoring the Square class to make it better in terms of encapsulation. What changes will you make to this class?*/
+
+//Add a calculateArea method:
+/* private void calculateArea(){
+this.area = this.side*this.side; }
+ */
+
+//Make side and area fields private.
+
+/*
+Change setSide method to:
+public void setSide(double d){
+this.side = d; calculateArea(); }
+ */
+
+
+
+//Make the getArea method public.
+
+/**There can be multiple ways to accomplish this. The exam asks you questions on the similar pattern.
+ The key is that your data variable should be private and the functionality that is to be exposed outside should be public.
+ Further, your setter methods should be coded such that they don't leave the data members inconsistent with each other.*/
+
