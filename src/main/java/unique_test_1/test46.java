0dcf7e46c72e7d4185ec554c19daeb43cd082b0b
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-09-10 23:55:06 +0200

    update

---
+++ src/main/java/unique_test_1/test46.java
@@ -0,0 +1,13 @@
+package unique_test_1;
+
+public class test46 {
+}
+/*Identify correct constructs.*/
+
+
+/*try {
+  for( ;; );
+}finally { }
+A try block must be accompanied by either a catch block or a finally block or both.**/
+
+/**Note that a try with resources block may or may not to have any catch or finally block at all. However, try with resources is not in scope for this exam.*/
