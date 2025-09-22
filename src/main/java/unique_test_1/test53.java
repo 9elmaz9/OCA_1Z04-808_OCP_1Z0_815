ee62d99507e2db568a74eff0db810ff43a59c2c1
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-09-23 00:10:48 +0200

    update

---
+++ src/main/java/unique_test_1/test53.java
@@ -0,0 +1,13 @@
+package unique_test_1;
+
+public class test53 {
+}
+/**Which of the following is/are valid instantiations and initializations of a multi dimensional array?*/
+
+/**int[] array2D[] = new int [2] [2]; array2D[0] [0] = 1; array2D[0] [1] = 2; array2D[1] [0] = 3;*/
+
+//Notice that element [1][1] is not given a value explicitly in the code. It is given a default value of 0 automatically.
+
+//int[] arr = {1, 2}; int[][] arr2 = {arr, {1, 2}, arr}; int[][][] arr3 = {arr2};
+
+
