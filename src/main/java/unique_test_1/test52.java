04e279fee30fbd9aab2d36e3b30269d03de5777a
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-09-23 00:05:40 +0200

    update

---
+++ src/main/java/unique_test_1/test52.java
@@ -0,0 +1,16 @@
+package unique_test_1;
+
+public class test52 {
+}
+/**
+ Given the following declaration, select the correct way to get the number of elements in the array, assuming that the array has been initialized.
+
+ int[] intArr;*/
+
+
+//intArr.length
+
+//Each array object has a member variable named public final length of type 'int' that contains the size of the array.
+
+//FYI, All types of arrays are objects. i.e. intArr instanceof Object is true.
+
