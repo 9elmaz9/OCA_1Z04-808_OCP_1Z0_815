87298e07ad10369b1bb5d353932f833744b5e822
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-09-22 23:59:56 +0200

    enthuware

---
+++ src/main/java/unique_test_1/test50.java
@@ -0,0 +1,22 @@
+package unique_test_1;
+
+public class test50 {
+}
+/**
+ A new Java programmer has written the following method that takes an array of integers and sums up all the integers that are less than 100.
+
+
+ public void processArray(int[] values){
+ int sum = 0;
+ int i = 0;
+ try{
+ while(values[i]<100){
+ sum = sum +values[i];
+ i++;
+ }
+ }
+ catch(Exception e){ /* Ignore */ }
+        System.out.println("sum = "+sum);
+        }
+
+        Which of the following are best practices to improve this code?
