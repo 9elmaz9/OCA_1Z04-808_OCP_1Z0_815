a667af5309e85c632aa16bad41674f18efb577e4
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-09-30 00:02:08 +0200

    update

---
+++ src/main/java/Extra/unique_test_2/test2.java
@@ -0,0 +1,15 @@
+package Extra.unique_test_2;
+
+public class test2 {
+}
+//Which of the following statements will correctly create and initialize an array of Strings to non null elements?
+
+
+
+//String[] sA = new String[] { "aaa"};
+
+//String[] sA = new String[1] ; sA[0] = "aaa";
+
+//String[] sA = {new String( "aaa")};
+
+//String[] sA = { "aaa"};
