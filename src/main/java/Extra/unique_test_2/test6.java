026e2d65ab286acb081854df4a52178d8ee0aaf4
Dzhelianchyk <kakbyelmaz@gmail.com>
2025-10-04 23:39:19 +0200

    update

---
+++ src/main/java/Extra/unique_test_2/test6.java
@@ -0,0 +1,11 @@
+package Extra.unique_test_2;
+
+public class test6 {
+}
+//byte and int both hold signed values. So when b is assigned to i, the sign is preserved.
+
+
+//final int x = 10;
+
+
+//    static int x = 10;
